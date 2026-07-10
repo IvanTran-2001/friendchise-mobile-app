06e9edf53b48e56c379c2f74feb64fc89ba25db0
Ivan Tran <mystoganx2001@gmail.com>
2026-07-10 22:49:25 +1000

    fix: restore shared org profile helper

---
+++ components/layout/profile-panel/organizations-shared.ts
@@ -0,0 +1,15 @@
+import { apiFetch } from "../../../src/lib/api/client";
+
+export type Org = {
+  id: string;
+  name: string;
+  image?: string | null;
+};
+
+type OrgResponse = {
+  organizations: Org[];
+};
+
+export async function fetchOrganizations() {
+  return apiFetch<OrgResponse>("/api/mobile/me/organizations");
+}
