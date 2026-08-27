28fc76bfd4ef456a02164dba0df6827b19a4d782
Ivan Tran <mystoganx2001@gmail.com>
2026-08-27 18:22:18 +1000

    Complete Expo auth sessions on startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
 import { Stack } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function RootLayout() {
   return (
