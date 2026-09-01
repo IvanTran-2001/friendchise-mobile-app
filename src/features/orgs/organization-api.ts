205813bdd680bc79beec00768e09b09045227a32
Ivan Tran <mystoganx2001@gmail.com>
2026-09-01 21:00:56 +1000

    Revert "Wire mobile org create and join flows"
    This reverts commit 615c29cc4a53929878d248ab62f3516f0425d5bc.

--- src/features/orgs/organization-api.ts
+++ src/features/orgs/organization-api.ts
@@ -10,46 +10,6 @@ export type OrgResponse = {
   organizations: Org[];
 };
 
-export type CreateOrganizationInput = {
-  title: string;
-  timezone?: string;
-  address?: string;
-  operatingDays?: string[];
-  openTimeMin?: number;
-  closeTimeMin?: number;
-};
-
-export type CreateOrganizationResponse = {
-  organization: Org;
-};
-
-export type JoinOrganizationInput = {
-  token: string;
-  timezone?: string;
-  address?: string;
-  operatingDays?: string[];
-  openTimeMin?: number;
-  closeTimeMin?: number;
-};
-
-export type JoinOrganizationResponse = {
-  organization: Org;
-};
-
 export async function fetchOrganizations() {
   return apiFetch<OrgResponse>("/api/mobile/me/organizations");
-}
-
-export async function createOrganization(input: CreateOrganizationInput) {
-  return apiFetch<CreateOrganizationResponse>("/api/mobile/me/organizations", {
-    method: "POST",
-    body: JSON.stringify(input),
-  });
-}
-
-export async function joinOrganization(input: JoinOrganizationInput) {
-  return apiFetch<JoinOrganizationResponse>("/api/mobile/me/organizations/join", {
-    method: "POST",
-    body: JSON.stringify(input),
-  });
 }
