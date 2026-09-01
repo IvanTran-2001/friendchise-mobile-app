cf0c8659404ed7077e0245afe81b67435410b3a7
Ivan Tran <mystoganx2001@gmail.com>
2026-09-01 21:45:41 +1000

    fix org hub component imports

--- src/features/orgs/org-hub-screen.tsx
+++ src/features/orgs/org-hub-screen.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import { CirclePlus, LogIn, Network } from "lucide-react-native";
-import { Card } from "../../components/ui/card";
-import { ListRow } from "../../components/ui/list-row";
-import { Screen } from "../../components/ui/screen";
-import { ScreenHeader } from "../../components/ui/screen-header";
+import { Card } from "../../../components/ui/card";
+import { ListRow } from "../../../components/ui/list-row";
+import { Screen } from "../../../components/ui/screen";
+import { ScreenHeader } from "../../../components/ui/screen-header";
 import { colors, spacing } from "../../lib/theme";
 
 export function OrgsHubScreen() {
